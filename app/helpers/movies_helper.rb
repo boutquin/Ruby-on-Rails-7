efd1af5d32f7d32103041da7aba87067a103c5ed
boutquin <pierre@boutquin.com>
2024-09-21 18:56:39 -0400

    After View Helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,19 @@
 module MoviesHelper
+  # Returns the formatted gross for a movie
+  def total_gross(movie)
+    if movie.flop?
+      "Flop!"
+    else
+      movie.
+    end
+  end
+
+  # Returns the formatted release date for a movie
+  def year_of(movie)
+    if movie.released_on.nil?
+      "N/A"
+    else
+      movie.released_on.year
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  def flop?
+    # Return true if the total_gross is blank or less than 225_000_000
+    total_gross.blank? || total_gross < 225_000_000
+  end
 end
